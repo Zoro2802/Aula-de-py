b3a746a56f187eff775e47d2de9e98868ccbdf7e
wesley <Wesleylenilson1979@gmail.com>
2025-09-02 15:12:46 -0300

    Tabuada e primo

--- Matheus/Par e impar/Main.py
+++ Matheus/Par e impar/Main.py
@@ -1,7 +1,7 @@
 def main():
     num = int (input("digete um numero: "))
     
-    if num %2 == 0:
+    if num % 2 == 0:
         print("O número", num, " é par")
     else:
         print("O número", num, " é impar")
---
+++ Matheus/Par e impar/Primo/main.py
@@ -0,0 +1,18 @@
+def main():
+
+    num = int (input("Digete um número: "))
+
+    i = 2
+
+    while num != i and num > 1:
+        if num % i == 0:
+            break
+        i += 1
+
+    if num ==i:
+        print("O numero ", num," é primo")
+    else:
+        print("O número ", num, " não é primo")
+
+    return 0
+main()
